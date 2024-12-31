1fb99390f9c74535ab13441426c85e6bb317a765
frsyi <fahmarosyi@gmail.com>
2024-12-31 13:53:47 +0700

    menambahkan method DeleteGround pada GroundManager

--- Assets/Scripts/GroundManager.cs
+++ Assets/Scripts/GroundManager.cs
@@ -7,15 +7,17 @@ public class GroundManager : MonoBehaviour
     public GameObject[] groundPrefabs;
     public float zSpawn = 0;
     public float groundLength = 30;
-    public int numberOfGround = 5;
+    public int numberOfGround = 3;
+    private List<GameObject> activeGrounds = new List<GameObject>();
+    public Transform playerTransform;
 
     void Start()
     {
         for (int i = 0; i < numberOfGround; i++)
-        { 
+        {
             if (i == 0)
             {
-                SpawnGround(0);
+                SpawnGround(0); 
             }
             else
             {
@@ -26,12 +28,23 @@ public class GroundManager : MonoBehaviour
 
     void Update()
     {
-        
+        if (playerTransform.position.z -35 > zSpawn - (numberOfGround * groundLength))
+        {
+            SpawnGround(Random.Range(0, groundPrefabs.Length));
+            DeleteGround(); 
+        }
     }
 
     public void SpawnGround(int groundIndex)
     {
-        Instantiate(groundPrefabs[groundIndex], transform.forward * zSpawn, transform.rotation);
+        GameObject newGround = Instantiate(groundPrefabs[groundIndex], transform.forward * zSpawn, transform.rotation);
+        activeGrounds.Add(newGround);
         zSpawn += groundLength;
+    }
+
+    public void DeleteGround()
+    {
+        Destroy(activeGrounds[0]);
+        activeGrounds.RemoveAt(0);
     }
 }
